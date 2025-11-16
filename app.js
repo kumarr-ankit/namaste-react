ecb68aab8d493f540c8a1989a4e1c33693625ec5
Ankit Kumar <ankitkr5447@gmail.com>
2025-11-17 00:58:06 +0530

    added some Info on Parcel

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-console.log(React);
-//Remove comments
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+console.log(ReactDOM)
+console.log(React)
+// //Remove comments
 let content = `
         I am bold tag
         `;
 const heading = React.createElement("h1", { id: "heading" }, content); // here object : help to passs attributes like id , classes etc
-const root = ReactDOM.createRoot(document.getElementById("root"));
+ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const parent = React.createElement(
   "div",
@@ -15,12 +19,12 @@ const parent = React.createElement(
     {
       id: "para",
     },
-    ["Ankit Is Nice Boy", heading,heading]
+    ["Ankit Kumar is Bad Boy", heading,heading]
   )
 );
 
 //render
 root.render(heading);
 
-root.render(parent);
+// root.render(parent);
 console.log(heading);
---
+++ dist/react.js.6bd02f5a.js
@@ -0,0 +1,25 @@
+impo;
+console.log(React);
+//Remove comments
+let content = `
+        I am bold tag
+        `;
+const heading = React.createElement("h1", {
+    id: "heading"
+}, content); // here object : help to passs attributes like id , classes etc
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const parent = React.createElement("div", {
+    id: "parent"
+}, React.createElement("p", {
+    id: "para"
+}, [
+    "Ankit Kumar is Bad Boy",
+    heading,
+    heading
+]));
+//render
+root.render(heading);
+root.render(parent);
+console.log(heading);
+
+//# sourceMappingURL=react.js.6bd02f5a.js.map
