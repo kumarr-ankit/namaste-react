5141e4663c9cb601ae6e7ad893f24b80afc9e0a1
Ankit Kumar <ankitkr5447@gmail.com>
2025-11-17 00:07:39 +0530

    installed parcel

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 console.log(React);
-
+//Remove comments
 let content = `
         I am bold tag
         `;
 const heading = React.createElement("h1", { id: "heading" }, content); // here object : help to passs attributes like id , classes etc
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+
+  React.createElement(
+    "p",
+    {
+      id: "para",
+    },
+    ["Ankit Is Nice Boy", heading,heading]
+  )
+);
+
 //render
 root.render(heading);
+
+root.render(parent);
 console.log(heading);
